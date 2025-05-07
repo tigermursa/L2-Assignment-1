f58442fbd672cbb5984e5637383b25568d579e47
Mursalin <mursalin2311@gmail.com>
2025-05-08 01:17:03 +0600

    problem 2 solved

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@
 
 function formatString(input: string, toUpper?: boolean): string {
   if (toUpper === false) {
-    console.log("level-1");
     return input.toLocaleLowerCase();
   } else {
-    console.log("level-3");
     return input.toUpperCase();
   }
 }
@@ -13,7 +11,31 @@ function formatString(input: string, toUpper?: boolean): string {
 //console.log(formatString("mursalin", true));
 
 //!Problem-2
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book A", rating: 4.0 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+type input = {
+  items: { title: string; rating: number }[];
+};
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  const newArray = items.filter((item) => item.rating >= 4);
+  return newArray;
+}
+
+//console.log(filterByRating(books));
+
 //!Problem-3
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {}
+
 //!Problem-4
 //!Problem-5
 //!Problem-6
