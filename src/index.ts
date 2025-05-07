5d7e3496f87c738bcbc7c35e79cc5bb8e310065c
Mursalin <mursalin2311@gmail.com>
2025-05-07 22:58:38 +0600

    problem one solved

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello");
+//!Problem-1
+
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper === false) {
+    console.log("level-1");
+    return input.toLocaleLowerCase();
+  } else {
+    console.log("level-3");
+    return input.toUpperCase();
+  }
+}
+
+//console.log(formatString("mursalin", true));
+
+//!Problem-2
+//!Problem-3
+//!Problem-4
+//!Problem-5
+//!Problem-6
+//!Problem-7
+//!Problem-8
